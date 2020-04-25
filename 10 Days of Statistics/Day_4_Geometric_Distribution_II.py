dc91ce5d846632702923cd7e4d62f2450f802f2a
Luiz Bonfim <63732228+Bonfim-luiz@users.noreply.github.com>
2020-04-25 11:47:46 -0300

    Inclusão de arquivo.

---
+++ 10 Days of Statistics/Day_4_Geometric_Distribution_II.py
@@ -0,0 +1,25 @@
+
+# Day 4: Geometric Distribution II
+# https://www.hackerrank.com/challenges/s10-geometric-distribution-2/problem
+
+# Resultado do problema proposto
+n = 5
+p = 1/3
+resultado = 0
+while n > 0:
+    formula = (1-p)**(n-1)*(p)
+    resultado += formula
+    n = n - 1
+print(round(resultado,3))
+
+# Automatizando o resultado
+
+n = int(input())
+entrada = list(map(int,input().split()))
+p = entrada[0]/entrada[1]
+resultado = 0
+while n > 0:
+    formula = (1-p)**(n-1)*(p)
+    resultado += formula
+    n = n - 1
+print(round(resultado,3))
